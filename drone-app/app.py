4e001cbf6df3629424e5d6501deebddaa821ac9d
Craig Dunk <craig@goproperly.com>
2019-03-04 00:17:40 -0500

    stub of duty cycle calculation

--- drone-app/app.py
+++ drone-app/app.py
@@ -2,27 +2,42 @@
 
 import time
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.OUT)
 
-p = GPIO.PWM(12, 50)  # channel=12 frequency=50Hz
+MAX_WIDTH_MILLIS = 2
+MIN_WIDTH_MILLIS = 1
+PULSE_PERIOD =20
+
+def duty_cycle_from_percent(percent_speed:float):
+    something = 5.0
+    return something
+
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+
+p = GPIO.PWM(18, 50)  # channel=12 frequency=50Hz
 p.start(0)
 
 p.ChangeDutyCycle(5)
-time.sleep(5)
+time.sleep(60)
 
-try:
-    while True:
-        for dc in range(60, 90, 1):
-            p.ChangeDutyCycle(dc/10.0)
-            time.sleep(0.5)
-        for dc in range(90, 60, -1):
-            p.ChangeDutyCycle(dc/10.0)
-            time.sleep(0.5)
-except KeyboardInterrupt:
-    pass
+print("Done Init")
+#try:
+#    while True:
+#        for dc in range(60, 90, 1):
+#            p.ChangeDutyCycle(dc/10.0)
+#            time.sleep(0.1)
+#        for dc in range(90, 60, -1):
+#            p.ChangeDutyCycle(dc/10.0)
+#            time.sleep(0.1)
+#except KeyboardInterrupt:
+#    pass
+#print("cleaning up")
 p.stop()
 GPIO.cleanup()
+
+
 
 
 
