e495e266b4f0fa3f95730edb02fcfa7d299c8647
Craig Dunk <craig@goproperly.com>
2019-03-05 00:17:27 -0500

    create separate systems, add initializers and stub functions with comments

---
+++ drone-app/test_flight_controller.py
@@ -0,0 +1,56 @@
+
+import time
+from drone import Drone
+
+#NOTE: this controller waits 30 seconds, rises for 5 seconds at low rate, then proceeds down 10 seconds, and repeats
+
+class TestFlightController (object):
+
+    def __init__(self, drone_to_control):
+        self.drone_to_control = drone_to_control
+        self.current_state = "WAIT"
+        self.enter_state_time = time.time()
+        self.exit_state_time = self.enter_state_time + 30
+
+    def process_actions(self):
+
+        time_now = time.time()
+
+        if (time_now < self.enter_state_time):
+            # over cautious code that handles clock changes
+            # i.e. handles time going backwards
+            self.enter_state_time = time_now
+            self.exit_state_time = time_now
+
+        if (time_now < self.exit_state_time):
+            # nothing to do yet
+            return
+
+        #proceed to next state:
+        self.enter_state_time = self.exit_state_time
+
+        # TODO: proper declarative state machine...
+        if self.current_state == "WAIT":
+            self.current_state = "RISE"
+            self.exit_state_time = self.enter_state_time + 5
+            self.drone_to_control.rise_at_rate(10)
+
+        if self.current_state == "RISE":
+            self.current_state = "DROP"
+            self.exit_state_time = self.enter_state_time + 10
+            self.drone_to_control.rise_at_rate(-10)
+
+        if self.current_state == "DROP":
+            self.current_state = "WAIT"
+            self.exit_state_time = self.enter_state_time + 30
+            self.drone_to_control.rise_at_rate(0)
+
+
+
+
+
+
+
+
+
+
