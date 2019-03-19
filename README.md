f47eeab3aca027f184e51767e2d0b5515d70fc18
benjamin dunk <benkdunk@gmail.com>
2019-03-19 09:41:23 -0400

    moved the copyright thing from i2c into the README, change if this is not the correct thing to do

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # drone-controller
+
+
+### MPU 9250 Interface Derived from https://github.com/PiStuffing/Quadcopter/blob/master/Quadcopter.py
+##
+##
+####################################################################################################
+####################################################################################################
+##                                                                                                ##
+## Hove's Raspberry Pi Python Quadcopter Flight Controller.  Open Source @ GitHub                 ##
+## PiStuffing/Quadcopter under GPL for non-commercial application.  Any code derived from         ##
+## this should retain this copyright comment.                                                     ##
+##                                                                                                ##
+## Copyright 2012 - 2018 Andy Baker (Hove) - andy@pistuffing.co.uk                                ##
+##                                                                                                ##
+####################################################################################################
+####################################################################################################
+
+
+
+
+####################################################################################################
+#
+#  Adafruit i2c interface enhanced with performance / error handling enhancements
+#
+####################################################################################################
--- drone-app/i2c.py
+++ drone-app/i2c.py
@@ -1,28 +1,4 @@
 
-
-### MPU 9250 Interface Derived from https://github.com/PiStuffing/Quadcopter/blob/master/Quadcopter.py
-##
-##
-####################################################################################################
-####################################################################################################
-##                                                                                                ##
-## Hove's Raspberry Pi Python Quadcopter Flight Controller.  Open Source @ GitHub                 ##
-## PiStuffing/Quadcopter under GPL for non-commercial application.  Any code derived from         ##
-## this should retain this copyright comment.                                                     ##
-##                                                                                                ##
-## Copyright 2012 - 2018 Andy Baker (Hove) - andy@pistuffing.co.uk                                ##
-##                                                                                                ##
-####################################################################################################
-####################################################################################################
-
-
-
-
-####################################################################################################
-#
-#  Adafruit i2c interface enhanced with performance / error handling enhancements
-#
-####################################################################################################
 class I2C:
 
     def __init__(self, address, bus=smbus.SMBus(1)):
