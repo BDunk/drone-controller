fdcf86f7b1cd452329a8d0204baebed58a427dac
benjamin dunk <benkdunk@gmail.com>
2019-04-13 13:12:15 -0400

    stuff

--- tests/test_PID.py
+++ tests/test_PID.py
@@ -8,7 +8,7 @@ time_old=time.time()
 testPID.change_set_point(1)
 
 while time_difference < 10:
-    
+
     testPID.change_current_point(testPID.current_point-testPID.calculate())
 
     time_difference=time.time()-time_old
