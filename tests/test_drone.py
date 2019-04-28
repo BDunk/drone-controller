b005a094b852e78dfbaecd012467916c3378a08d
benjamin dunk <benkdunk@gmail.com>
2019-04-28 17:53:20 -0400

    created test drone

---
+++ tests/test_drone.py
@@ -0,0 +1,39 @@
+from tests.drone_emulator import DroneEmulator
+from unittest import TestCase
+import time
+import logging
+import sys
+import csv
+
+
+
+
+
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logger = logging.getLogger()
+
+logger.setLevel(logging.INFO)
+
+
+class TestSensorData(TestCase):
+
+
+
+    #Extension of the TestCase class, called once per test in this class
+    def setUp(self):
+        self.pid_response_handle = open('pid_response.csv', 'w')
+        self.pid_response_writer = csv.writer(self.pid_response_handle)
+        #self.pid_response_writer.writerow(['timestamp','value_at_timestamp'])
+
+
+    def tearDown(self):
+        if self.pid_response_handle:
+            self.pid_response_writer = None
+            self.pid_response_handle.close()
+            self.pid_response_handle = None
+
+
+
+
+    def test_drone(self):
